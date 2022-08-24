f45df2fd8c5877baf2befeff05e1ec7fb235abc6
OdubanjoJoshua <Joshuaodubanjo@yahoo.com>
2022-08-24 17:28:38 +0100

    'core'

--- core/settings.py
+++ core/settings.py
@@ -77,23 +77,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME':'burgermandb',
-#         'USER':'postgresbking',
-#         # 'USER':'postgres',
-#         'PASSWORD':'dove24',
-#         'HOST': '127.0.0.1',
-#         'PORT':'5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'burgermandb',
+        'USER':'postgresbking',
+        # 'USER':'postgres',
+        'PASSWORD':'dove24',
+        'HOST': '127.0.0.1',
+        'PORT':'5432'
+    }
+}
 
 # DATABASES = {
 #     'default': {
